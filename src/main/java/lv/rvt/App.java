78ce5a36d9b215395737fe64834d831d92f85614
23DP4STerm <166484773+23DP4STerm@users.noreply.github.com>
2024-09-27 10:51:22 +0000

    Programming exercise: Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,10 +7,25 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give speed:");
-        double number = Double.valueOf(scanner.nextLine());
-        if (number > 120) {
-            System.out.println("Speeding ticket!");
+
+        System.out.println("Give points [0-100]:");
+        double score = Double.valueOf(scanner.nextLine());
+        if (score > 100) {
+            System.out.println("Grade: incredible!");
+        } else if (score < 100 && score > 90) {
+            System.out.println("Grade: 5");
+        } else if (score < 89 && score > 80) {
+            System.out.println("Grade: 4");
+        } else if (score < 79 && score > 70) {
+            System.out.println("Grade: 3");
+        } else if (score < 69 && score > 60) {
+            System.out.println("Grade: 2");
+        } else if (score < 59 && score > 50) {
+            System.out.println("Grade: 1");
+        } else if (score < 49 && score > 0) {
+            System.out.println("Grade: failed");
+        } else {
+            System.out.println("Grade: impossible!");
         }
 
 
