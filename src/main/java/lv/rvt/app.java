5961ae2e3f9e2795fb94152ae7ee5c6da2e7017b
23DP4STerm <166484773+23DP4STerm@users.noreply.github.com>
2024-11-01 10:44:28 +0000

    Exercise 2 — Two Arrays

--- src/main/java/lv/rvt/app.java
+++ src/main/java/lv/rvt/app.java
@@ -2,20 +2,23 @@ package lv.rvt;
 import java.util.*;
 
 
-
-
-public class app {
-
+    class Exercise2 {
         public static void main(String[] args) {
-          int[] val = {0, 1, 2, 3}; 
-          int sum = 0; 
+          int[] val = {13, -4, 82, 17}; 
+          int[] twice = new int[val.length];
+          
+          System.out.println("Original Array: " 
+              + val[0] + " " + val[1] + " " + val[2] + " " + val[3]);
       
+          
           for (int i = 0; i < val.length; i++) {
-            sum += val[i];
+            twice[i] = 2 * val[i];
           }
       
-          System.out.println("Sum of all numbers = " + sum);
+          System.out.println("New Array: " 
+              + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3]);
         }
-      }
+    }
+
       
     
